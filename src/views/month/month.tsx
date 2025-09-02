36ee76c174e291b3594ff8a2b1024fc39a70078b
Vinicius <vinii.d3v@gmail.com>
2025-09-01 22:30:49 -0300

    mensal update

--- src/views/month/month.tsx
+++ src/views/month/month.tsx
@@ -26,7 +26,7 @@ export function Month() {
 
   useEffect(() => {
     async function fetchMensal() {
-      const res = await axios.get("https://trufapp-backend-6km2.onrender.com");
+      const res = await axios.get("https://trufapp-backend-6km2.onrender.com/mensal");
       setMonths(res.data);
     }
 
